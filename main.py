354a126db631fcad6d16207964cd76bc8156ffd9
Paul "Rohja" LESELLIER <rohja@rohja.com>
2021-01-07 14:18:23 +0100

    Fix typo

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ import threading
 
 while True:
     keyboard.wait("enter")
-    print("You pressed ented")
+    print("You pressed enter")
     # playsound('sound.mp3')
     threading.Thread(target=playsound, args=('sound.mp3',)).start()
